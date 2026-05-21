0d2a846665e7e91616b336aeb30fbb382fc09015
phalmeida05 <pedroohenriquealmeida05@gmail.com>
2026-05-21 11:38:00 -0300

    Adiciona ValueEventListener para preparar e fazer a lista de comodos e eletros.

--- app/src/main/java/com/example/wattwiseapp/listAppliance.java
+++ app/src/main/java/com/example/wattwiseapp/listAppliance.java
@@ -14,6 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.database.DatabaseError;
 
 public class listAppliance extends AppCompatActivity {
 
@@ -31,6 +32,45 @@ public class listAppliance extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // listagem
+
+        com.google.firebase.auth.FirebaseUser currentUser = com.google.firebase.auth.FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+
+            com.google.firebase.database.DatabaseReference eletrosRef = com.google.firebase.database.FirebaseDatabase.getInstance()
+                    .getReference("Usuarios")
+                    .child(userId)
+                    .child("Eletronicos");
+
+
+            eletrosRef.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
+                @Override
+                public void onDataChange(com.google.firebase.database.DataSnapshot snapshot) {
+
+
+
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                    android.util.Log.e("FirebaseError", "Erro ao buscar Eletrodomésticos: " + error.getMessage());
+
+                }
+
+
+            });
+
+        }
+
+
+
+
+
     }
 
     // menu
@@ -79,7 +119,5 @@ public class listAppliance extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    // listar pedro
 
 }
--- app/src/main/java/com/example/wattwiseapp/listRoom.java
+++ app/src/main/java/com/example/wattwiseapp/listRoom.java
@@ -2,13 +2,8 @@ package com.example.wattwiseapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -19,6 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.database.DatabaseError;
 
 public class listRoom extends AppCompatActivity {
 
@@ -39,6 +35,40 @@ public class listRoom extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        // listagem
+
+        com.google.firebase.auth.FirebaseUser currentUser = com.google.firebase.auth.FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+
+            com.google.firebase.database.DatabaseReference comodosRef = com.google.firebase.database.FirebaseDatabase.getInstance()
+                    .getReference("Usuarios")
+                    .child(userId)
+                    .child("Comodos");
+
+
+            comodosRef.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
+                @Override
+                public void onDataChange(com.google.firebase.database.DataSnapshot snapshot) {
+
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+
+                    android.util.Log.e("FirebaseError", "Erro ao buscar cômodos: " + error.getMessage());
+
+                }
+
+
+            });
+
+        }
+
     }
 
     // menu
